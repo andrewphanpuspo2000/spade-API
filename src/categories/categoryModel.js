335f5054b4c9ff50e7c4c5ac9150f9825e49363c
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-09-03 17:38:14 +1000

    update

--- src/categories/categoryModel.js
+++ src/categories/categoryModel.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 const categorySchema = mongoose.model("categories", {});
+const productSchema = mongoose.model("products", {});
 
 export const getAllCategories = () => {
   return categorySchema.find();
+};
+
+export const getCatItems = (id) => {
+  const _id = new mongoose.Types.ObjectId(id);
+  return productSchema.find({ parentCat: _id });
+};
+export const getOneCatItem = (id) => {
+  return productSchema.findById(id).then((data) => {
+    if (!data) {
+      throw new Error("no data");
+    }
+    return data;
+  });
 };
--- src/router/categoryRouter.js
+++ src/router/categoryRouter.js
@@ -1,7 +1,50 @@
 import express from "express";
-import { getAllCategories } from "../categories/categoryModel.js";
+import {
+  getAllCategories,
+  getCatItems,
+  getOneCatItem,
+} from "../categories/categoryModel.js";
 
 const router = express.Router();
+
+router.get("/getItem/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log("Data is ......................", id);
+    if (id) {
+      const data = await getOneCatItem(id);
+      console.log("data is ", data);
+      if (data) {
+        res.json({
+          status: "success",
+          message: "Item has been retrieved",
+          data,
+        });
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:slug/:_id", async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    if (_id) {
+      const data = await getCatItems(_id);
+      // console.log(data);
+      if (data) {
+        res.json({
+          status: "success",
+          message: "cat items retrieved",
+          data: data,
+        });
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/", async (req, res, next) => {
   try {
